77c45ba17a189e90d6af34cf77906abbb5c806e1
ismail harmanda <iharmanda@hotmail.com>
2021-11-24 01:07:34 +0300

    defining Generic types for useState

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
 const GuestList: React.FC = () => {
-  const [name, setName] = useState("");
+  const [name, setName] = useState<string>("");
+  const [guests, setGuests] = useState<string[]>([]);
+  const onClick: () => void = () => {
+    setName("");
+    setGuests((prev) => [...prev, name]);
+  };
   return (
     <div>
       <h3>Guest List</h3>
       <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button>Add Guest</button>
+      <button onClick={onClick}>Add Guest</button>
     </div>
   );
 };
